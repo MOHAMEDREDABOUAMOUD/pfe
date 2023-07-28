8d22f826f6631337bf0a8472b51291da51a21ed9
MOHAMEDREDABOUAMOUD <mohamedredabouamoud@gmail.com>
2023-07-28 20:47:18 +0100

    fix connexion to databse "pfe"

--- back-app/public/js/dao/db.js
+++ back-app/public/js/dao/db.js
@@ -4,10 +4,11 @@ import { createPool } from 'mysql';
 
 // Create the database connection pool
 const pool = createPool({
-  host: 'your_database_host',
-  user: 'your_database_user',
-  password: 'your_database_password',
-  database: 'your_database_name',
+    host : '127.0.0.1',
+    port : '3306',
+    database : 'pfe',
+    user : 'root',
+    password : ''
 });
 
 export default pool;
